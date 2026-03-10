a0cd0c1d133ad55e18ad6210efe0db4ae51f39a0
Илья <ilya.polstyanoy@icloud.com>
2026-03-11 02:12:05 +0300

    feat #19: Добавлено логирование в класс ByBitLoaderMarketDataImpl.java

--- market-data/market-data-bybit/src/main/java/io/github/ijlijapol/ByBitLoaderMarketDataImpl.java
+++ market-data/market-data-bybit/src/main/java/io/github/ijlijapol/ByBitLoaderMarketDataImpl.java
@@ -14,6 +14,7 @@ import io.github.ijlijapol.data.market.model.responce.CandleDTO;
 import io.github.ijlijapol.data.market.model.responce.CandlesDTO;
 import io.github.ijlijapol.mapper.MapperByBitData;
 import io.github.ijlijapol.mapper.MapperTimeFrame;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -22,6 +23,7 @@ import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 public class ByBitLoaderMarketDataImpl implements LoaderMarketData {
 
     private final BybitApiMarketRestClient client;
@@ -32,6 +34,8 @@ public class ByBitLoaderMarketDataImpl implements LoaderMarketData {
 
     @Override
     public CandlesDTO loadRecentMarketData(final RecentMarketData recentMarketData) {
+        log.info("Загрузка последних рыночных свеч: symbol-{}, timeFrame={}, lastTime={}",
+                recentMarketData.getSymbol(), recentMarketData.getTimeFrame(), recentMarketData.getLastTime());
 
         final Long startPeriod = getStartTimeFromLastTime(recentMarketData.getLastTime());
         final Long endDate = LocalDateTime.now(ZoneId.of("UTC")).toInstant(ZoneOffset.UTC).toEpochMilli();
@@ -46,7 +50,9 @@ public class ByBitLoaderMarketDataImpl implements LoaderMarketData {
                 .limit(1000)
                 .build();
 
+        log.debug("Первый запрос ByBit: start={}, end={}, limit=1000", startPeriod, endDate);
         final List<MarketKlineEntry> marketKlineEntries = MapperByBitData.convertFromResponse(client.getMarketLinesData(byBitRequest));
+        log.debug("Получено {} записей от ByBit", marketKlineEntries.size());
         final List<CandleDTO> candleDTOList = new ArrayList<>(MapperByBitData.convertFromMarketKlineEntry(marketKlineEntries));
 
         while (marketKlineEntries.size() == 1000) {
@@ -55,6 +61,7 @@ public class ByBitLoaderMarketDataImpl implements LoaderMarketData {
 
             final Long markerInteralLong = getTimeInMillisFromMarketInterval(marketInterval);
             final Long newStartTime = candleDTOList.getLast().getStartTime().toInstant(ZoneOffset.UTC).toEpochMilli() + markerInteralLong;
+            log.debug("Получено 1000 записей, запрашиваем следующую порцию начиная с newStartTime={}", newStartTime);
             final com.bybit.api.client.domain.market.request.MarketDataRequest newByBitRequest
                     = com.bybit.api.client.domain.market.request.MarketDataRequest.builder()
                     .category(CategoryType.SPOT)
@@ -67,8 +74,10 @@ public class ByBitLoaderMarketDataImpl implements LoaderMarketData {
 
             marketKlineEntries.addAll(MapperByBitData.convertFromResponse(client.getMarketLinesData(newByBitRequest)));
             candleDTOList.addAll(MapperByBitData.convertFromMarketKlineEntry(marketKlineEntries));
+            log.debug("Добавлено еще {} записей всего сейчас {}", marketKlineEntries.size(), candleDTOList.size());
         }
 
+        log.info("Загружено всего {} свечей за период с {} по {}", candleDTOList.size(), startPeriod, endDate);
         return CandlesDTO.builder()
                 .startPeriodTime(Instant.ofEpochMilli(startPeriod).atZone(ZoneId.of("UTC")).toLocalDateTime())
                 .endPeriodTime(LocalDateTime.now(ZoneOffset.UTC))
@@ -78,6 +87,12 @@ public class ByBitLoaderMarketDataImpl implements LoaderMarketData {
 
     @Override
     public CandlesDTO loadMarketDateForPeriodBetween(final MarketDataForPeriodBetween marketDataForPeriodBetween) {
+        log.info("Загрузка р=рыночных свечей за период: symbol={}, timeFrame={}, start={}, end={}",
+                marketDataForPeriodBetween.getSymbol(),
+                marketDataForPeriodBetween.getTimeFrame(),
+                marketDataForPeriodBetween.getStartTime(),
+                marketDataForPeriodBetween.getEndTime()
+        );
 
         final Long startPeriod = marketDataForPeriodBetween.getStartTime().toInstant(ZoneOffset.UTC).toEpochMilli();
         final Long endDate = marketDataForPeriodBetween.getEndTime().toInstant(ZoneOffset.UTC).toEpochMilli();
@@ -92,7 +107,9 @@ public class ByBitLoaderMarketDataImpl implements LoaderMarketData {
                 .limit(1000)
                 .build();
 
+        log.debug("Первый запрос ByBit: start={}, end={}, limit=1000", startPeriod, endDate);
         final List<MarketKlineEntry> marketKlineEntries = MapperByBitData.convertFromResponse(client.getMarketLinesData(byBitRequest));
+        log.debug("Получено {} записей от ByBit", marketKlineEntries.size());
         final List<CandleDTO> candleDTOList = new java.util.ArrayList<>(MapperByBitData.convertFromMarketKlineEntry(marketKlineEntries));
 
         while (marketKlineEntries.size() == 1000) {
@@ -101,6 +118,7 @@ public class ByBitLoaderMarketDataImpl implements LoaderMarketData {
 
             final Long markerInteralLong = getTimeInMillisFromMarketInterval(marketInterval);
             final Long newStartTime = candleDTOList.getLast().getStartTime().toInstant(ZoneOffset.UTC).toEpochMilli() + markerInteralLong;
+            log.debug("Получено 1000 записей, запрашиваем следующую порцию начиная с newStartTime={}", newStartTime);
             final com.bybit.api.client.domain.market.request.MarketDataRequest newByBitRequest
                     = com.bybit.api.client.domain.market.request.MarketDataRequest.builder()
                     .category(CategoryType.SPOT)
@@ -113,8 +131,10 @@ public class ByBitLoaderMarketDataImpl implements LoaderMarketData {
 
             marketKlineEntries.addAll(MapperByBitData.convertFromResponse(client.getMarketLinesData(newByBitRequest)));
             candleDTOList.addAll(MapperByBitData.convertFromMarketKlineEntry(marketKlineEntries));
+            log.debug("Добавлено еще {} записей всего сейчас {}", marketKlineEntries.size(), candleDTOList.size());
         }
 
+        log.info("Загружено всего {} свечей за период с {} по {}", candleDTOList.size(), startPeriod, endDate);
         return CandlesDTO.builder()
                 .startPeriodTime(marketDataForPeriodBetween.getStartTime())
                 .endPeriodTime(marketDataForPeriodBetween.getEndTime())
@@ -124,6 +144,10 @@ public class ByBitLoaderMarketDataImpl implements LoaderMarketData {
 
     @Override
     public CandleDTO loadLatestCandle(final MarketDataRequest marketDataRequest) {
+        log.info("Загрузка последней свечи: symbol={}, timeFrame={}",
+                marketDataRequest.getSymbol(),
+                marketDataRequest.getTimeFrame()
+        );
 
         final MarketInterval marketInterval = MapperTimeFrame.toMarketInterval(marketDataRequest.getTimeFrame());
         final Long endDate = LocalDateTime.now(ZoneOffset.UTC).toInstant(ZoneOffset.UTC).toEpochMilli();
@@ -138,14 +162,16 @@ public class ByBitLoaderMarketDataImpl implements LoaderMarketData {
                 .limit(1)
                 .build();
 
+        log.debug("Запрос последней свечи: start={}, end={}, limit=1", startTime, endDate);
         final List<MarketKlineEntry> marketKlineEntry = MapperByBitData.convertFromResponse(client.getMarketLinesData(byBitRequest));
-
         List<CandleDTO> candleDTOList = MapperByBitData.convertFromMarketKlineEntry(marketKlineEntry);
-        return candleDTOList.getFirst();
+        final CandleDTO candleDTO = candleDTOList.getFirst();
+        log.debug("Получена свеча: open={}, close={}", candleDTO.getOpenPrice(), candleDTO.getClosePrice());
+        return candleDTO;
     }
 
     private Long getStartTimeFromLastTime(final LastTime lastTime) {
-        return switch (lastTime) {
+        final Long startTime = switch (lastTime) {
             case DAY -> LocalDateTime.now(ZoneId.of("UTC")).minusDays(1).toInstant(ZoneOffset.UTC).toEpochMilli();
             case WEEK -> LocalDateTime.now(ZoneId.of("UTC")).minusWeeks(1).toInstant(ZoneOffset.UTC).toEpochMilli();
             case MONTH -> LocalDateTime.now(ZoneId.of("UTC")).minusMonths(1).toInstant(ZoneOffset.UTC).toEpochMilli();
@@ -155,10 +181,13 @@ public class ByBitLoaderMarketDataImpl implements LoaderMarketData {
             case FIVE_YEAR ->
                     LocalDateTime.now(ZoneId.of("UTC")).minusYears(5).toInstant(ZoneOffset.UTC).toEpochMilli();
         };
+
+        log.trace("Расчет startTime из lastTime={}, результат={}", lastTime, startTime);
+        return startTime;
     }
 
     private Long getTimeInMillisFromMarketInterval(final MarketInterval marketInterval) {
-        return switch (marketInterval) {
+        final Long timeInMillis = switch (marketInterval) {
             case ONE_MINUTE -> 60 * 1000L;
             case THREE_MINUTES -> 3 * 60 * 1000L;
             case FIVE_MINUTES -> 5 * 60 * 1000L;
@@ -173,5 +202,8 @@ public class ByBitLoaderMarketDataImpl implements LoaderMarketData {
             case WEEKLY -> 7 * 24 * 60 * 60 * 1000L;
             case MONTHLY -> 30 * 24 * 60 * 60 * 1000L;
         };
+
+        log.trace("Расчет timmeInMillis из marketInterval={}, результат={}", marketInterval, timeInMillis);
+        return timeInMillis;
     }
 }
