946fb1fa72f1210f91cb66fc4cd6afd6d26361b0
Илья <iljiafenix@yandex.ru>
2026-03-11 04:24:39 +0300

    Revert "feat(#20 ): Написаны юнит тесты для имеющихся методов" (#25)
    This reverts commit 9973084f102110483287e71101eb96059c6bb0a9.

--- market-data/market-data-bybit/src/main/java/io/github/ijlijapol/exception/InvalidLastTimeException.java
+++
@@ -1,7 +0,0 @@
-package io.github.ijlijapol.exception;
-
-public class InvalidLastTimeException extends RuntimeException {
-    public InvalidLastTimeException(String message) {
-        super(message);
-    }
-}
