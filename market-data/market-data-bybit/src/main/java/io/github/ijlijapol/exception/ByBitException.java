dca7254b2d5c292a3661a9bb58fb771f896f7515
Илья <iljiafenix@yandex.ru>
2026-03-11 06:23:59 +0300

    Revert "Refactor/#22 optimize pagination and add error handling" (#28)
    This reverts commit e0801d32b79a6186461d817cea161d311b53224f.

--- market-data/market-data-bybit/src/main/java/io/github/ijlijapol/exception/ByBitException.java
+++
@@ -1,7 +0,0 @@
-package io.github.ijlijapol.exception;
-
-public class ByBitException extends RuntimeException {
-    public ByBitException(String message, Exception cause) {
-        super(message, cause);
-    }
-}
--- market-data/market-data-bybit/src/main/java/io/github/ijlijapol/exception/UncorrectedRequestByBit.java
+++
@@ -1,7 +0,0 @@
-package io.github.ijlijapol.exception;
-
-public class UncorrectedRequestByBit extends RuntimeException {
-    public UncorrectedRequestByBit(String message) {
-        super(message);
-    }
-}
