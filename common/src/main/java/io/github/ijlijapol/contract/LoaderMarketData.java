43a289945278d03378d67f1608bd73f6c8668c1b
Илья <ilya.polstyanoy@icloud.com>
2026-04-04 05:34:45 +0300

    refactor(#34): добавлена документаци к интерфейаву LoaderMarketData

--- common/src/main/java/io/github/ijlijapol/contract/LoaderMarketData.java
+++ common/src/main/java/io/github/ijlijapol/contract/LoaderMarketData.java
@@ -6,11 +6,33 @@ import io.github.ijlijapol.model.request.RecentMarketDataRequest;
 import io.github.ijlijapol.model.responce.CandleDTO;
 import io.github.ijlijapol.model.responce.CandlesDTO;
 
+/**
+ * Контракт для загрузки рыночных данных (свечей) из источника данных.
+ */
 public interface LoaderMarketData {
 
+    /**
+     * Загружает коллекцию последних рыночных свечей согласно параметрам запроса.
+     * Количество свечей и их таймфрейм определяются в объекте запроса.
+     *
+     * @param marketDataRequest запрос, содержащий параметры (символ, таймфрейм, за какое последнее время запрос)
+     * @return объект {@link CandlesDTO}, содержащий список запрошенных свечей
+     */
     CandlesDTO loadRecentMarketData(RecentMarketDataRequest marketDataRequest);
 
+    /**
+     * Загружает рыночные свечи за указанный временной период между двумя датами.
+     *
+     * @param marketDataRequest запрос, содержащий символ, таймфрейм, начальную и конечную даты периода
+     * @return объект {@link CandlesDTO}, содержащий свечи, попадающие в заданный интервал
+     */
     CandlesDTO loadMarketDateForPeriodBetween(MarketDataForPeriodBetweenRequest marketDataRequest);
 
+    /**
+     * Загружает последнюю (самую свежую) завершённую свечу.
+     *
+     * @param marketDataRequest запрос, содержащий символ и таймфрейм свечи
+     * @return объект {@link CandleDTO} с данными последней свечи
+     */
     CandleDTO loadLatestCandle(LastCandleRequest marketDataRequest);
 }
